705090587fb86310cb440d454c5b35ce2cda04bf
Thais Santos <thais@4bis.nl>
2018-12-06 10:57:50 +0100

    fixing scroll and adding last parts

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+$('.js-btn1').click(() => {
+    $('html, body').animate({
+        scrollTop: $('#how').offset().top - 75
+    }, 800);
+});
+$('.js-btn2').click(() => {
+    $('html, body').animate({
+        scrollTop: $('#about').offset().top - 75
+    }, 800);
+});
+$('.js-btn3').click(() => {
+    $('html, body').animate({
+        scrollTop: $('#roadmap').offset().top - 75
+    }, 800);
+});
+$('.js-btn4').click(() => {
+    $('html, body').animate({
+        scrollTop: $('#team').offset().top - 75
+    }, 800);
+});
+$('.js-btn5').click(() => {
+    $('html, body').animate({
+        scrollTop: $('#news').offset().top - 75
+    }, 800);
+});
+$('.js-btn6').click(() => {
+    $('html, body').animate({
+        scrollTop: $('#faq').offset().top - 75
+    }, 800);
+});
